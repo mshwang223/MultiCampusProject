9bd2a1d291a5074fafba5cd8b7595233f74ca7b8
xangjong <csj7080@naver.com>
2022-06-26 16:33:46 +0900

    update

--- Project/src/main/webapp/js/login.js
+++ Project/src/main/webapp/js/login.js
@@ -1,35 +1,28 @@
 /**
- * login.html에서 사용
- * 임시 사용자 id : abc1001,	pw: 1001
- * 임시 관리자 id : admin,		pw: admin
+ * 
  */
  
  $(document).ready(function(){
 	$('img').on('mouseover', function(){
-		$(this).attr('src', 'image/logo-w2.png').animate({top:-90},500)
+		$(this).attr('src', '../image/logo-w2.png').animate({top:-90},500)
 		
 	})
 	.on('mouseout', function(){
-		$(this).attr('src', 'image/logo-w.png').animate({top:-70},500)
+		$(this).attr('src', '../image/logo-w.png').animate({top:-70},500)
 		
 	})
 	.on('click', function(){
 		location.href = "메인메뉴";
 	});
 	
-
-	$('#loginBtn').on('click', function(){
-		
-		const id = $('#idBox').val();
-		const pw = $('#pwdBox').val();
-		
-		if(id == ""){
+	$('#loginForm').on('submit', function(){
+		if($('#idBox').val() == ""){
 			alert("아이디를 입력해주세요")
 			$('#idBox').focus();
 			return false;
 		}
 		
-		if(pw == ""){
+		if($('#pwdBox').val() == ""){
 			alert("비밀번호를 입력해주세요")
 			$('#pwdBox').focus();
 			return false;
@@ -40,7 +33,6 @@
 		
 		window.location.href="index.html";
 	});
-
 	
 	$('#join').on('click', function(){
 		var ask = confirm("화원가입을 하시겠습니까?");
