a086078acfffb7d827ae579cb0d12059870144fd
mshwang223 <mshwang223@gmail.com>
2022-06-26 19:15:59 +0900

    jaeyun commit!

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -3,7 +3,7 @@
  *  index.html 에서 사용
  */
  
-$(document).ready(function(){
+ $(document).ready(function(){
 	
 	// 회원 가입 버튼 클릭 시 페이지 이동
 	$('#join').on('click', function(){
@@ -40,16 +40,16 @@ $(document).ready(function(){
 	
 	// 일반 사용자일 경우(임시 사용자 id : 'abc1001' pw: '1011' 홍길동)
 	if(id == "abc1001" && pw == "1001") {
-		//let tmpHtml = `<h1 id="user">사용자 홍길동님 어서오세요.</h1>`;
-		//$('.searchBox').append(tmpHtml);
+		let tmpHtml = `<h1 id="user">사용자 홍길동님 어서오세요.</h1>`;
+		$('#userName').append(tmpHtml);
 		
 		$('#myPage').show();	// 로그인 한 사용자는 myPage 버튼 확인
 		
 		$('#login').hide();
 		$('#logout').show();
 	} else if(id == "admin" && pw == "admin") {
-		//let tmpHtml = `<h1 id="user">관리자님 어서오세요.</h1>`;
-		//$('.searchBox').append(tmpHtml);
+		let tmpHtml = `<h1 id="user">관리자님 어서오세요.</h1>`;
+		$('#userName').append(tmpHtml);
 		
 		$('#myPage').show();	// 로그인 한 사용자는 myPage 버튼 확인
 		$('#admin').show();		// 로그인 한 관리자는 사용자 관리 버튼 확인
