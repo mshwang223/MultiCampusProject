9c049b9ec68bdf6be6d7a2b65bc8995407ccab09
mshwang223 <mshwang223@gmail.com>
2022-06-27 13:27:49 +0900

    jaeyun commit!

--- src/main/webapp/js/slideShow.js
+++ src/main/webapp/js/slideShow.js
@@ -16,6 +16,24 @@ $(document).ready(function() {
 		$('#slidePanel').animate({'left':moveLeft}, 'slow');
 	}
 	
+	// prev 버튼 클릭하면 앞으로 이동
+	$('#prevBtn').on('click', function() {
+		// 현재 Index 위치 계산
+		if(movedIndex != 0)	// 첫 번째가 아니면
+			movedIndex = movedIndex - 1;	// 왼쪽으로 한 칸 이동
+			
+		moveSlide(movedIndex);	// 실제 이동하는 함수 호출 (인덱스값 전달)	
+	});
+	
+	// next 버튼 클릭하면 뒤로 이동
+	$('#nextBtn').on('click', function() {
+		// 현재 Index 위치 계산
+		if(movedIndex != 4)	// 마지막이 아니면
+			movedIndex = movedIndex + 1;	// 오른쪽으로 한 칸 이동
+			
+		moveSlide(movedIndex);	// 실제 이동하는 함수 호출 (인덱스값 전달)	
+	});
+	
 	
 	var randomNumber = Math.floor(Math.random() * 5);
 	moveSlide(randomNumber);
@@ -36,4 +54,6 @@ $(document).ready(function() {
 			moveSlide(index);	
 		});
 	});
+	
+	
 }); 
